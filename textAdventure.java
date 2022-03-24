5be1d841e04752c456fddf1f035f6097f09e7654
innshpi <innshpi@whs.school.nz>
2022-03-25 12:57:44 +1300

    starting files

---
+++ textAdventure.java
@@ -0,0 +1,28 @@
+
+/**
+ * Write a description of class textAdventure here.
+ *
+ * @author (Piper Inns Hall)
+ * @version (24/03/21)
+ */
+import java.util.Scanner;                    
+public class textAdventure
+{
+    // instance variables - replace the example below with your own
+    final int INVALIDDIRECTION = -1;
+    int[] goNorth = {/*room 0*/INVALIDDIRECTION,/*room 1*/INVALIDDIRECTION,/*room 2*/INVALIDDIRECTION};
+    int[] goSouth = {/*room 0*/INVALIDDIRECTION,/*room 1*/INVALIDDIRECTION,/*room 2*/INVALIDDIRECTION};
+    int[] goEast = {/*room 0*/INVALIDDIRECTION,/*room 1*/INVALIDDIRECTION,/*room 2*/INVALIDDIRECTION};
+    int[] goWest = {/*room 0*/1,/*room 1*/INVALIDDIRECTION,/*room 2*/INVALIDDIRECTION};
+    int[] goUp = {/*room 0*/INVALIDDIRECTION,/*room 1*/INVALIDDIRECTION,/*room 2*/1};
+    int[] goDown = {/*room 0*/INVALIDDIRECTION,/*room 1*/2,/*room 2*/INVALIDDIRECTION};
+    //int w-+[] roomDescript ={/*room 0*/"Grass patch",/*room 1*/"Sand dunes",/*room 2*/"Beware of cave Cave"};
+    /**
+     * Constructor for objects of class textAdventure
+     */
+    public textAdventure()
+    {
+        // initialise instance variables
+        
+    }
+}
